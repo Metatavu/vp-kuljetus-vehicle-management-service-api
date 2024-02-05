f450d4bc82980cf377bdc1e0ee01808568b06b1b
Katja Danilova <katja.danilova@protonmail.com>
2024-02-05 14:15:46 +0200

    remove towable telematic test(not implemented)

--- src/test/kotlin/fi/metatavu/vp/vehiclemanagement/test/functional/TruckTelematicDataTestIT.kt
+++ src/test/kotlin/fi/metatavu/vp/vehiclemanagement/test/functional/TruckTelematicDataTestIT.kt
@@ -5,7 +5,6 @@ import fi.metatavu.invalid.InvalidValueTestScenarioBuilder
 import fi.metatavu.invalid.InvalidValueTestScenarioPath
 import fi.metatavu.invalid.InvalidValues
 import fi.metatavu.vp.test.client.models.TelematicData
-import fi.metatavu.vp.test.client.models.Towable
 import fi.metatavu.vp.test.client.models.Truck
 import fi.metatavu.vp.vehiclemanagement.test.functional.settings.ApiTestSettings
 import io.quarkus.test.junit.QuarkusTest
@@ -27,14 +26,8 @@ class TruckTelematicDataTestIT : AbstractFunctionalTest() {
     )
 
     @Test
-    fun testCreate() = createTestBuilder().use {
+    fun testCreateTruckTelematicData() = createTestBuilder().use {
         val truck = it.user.trucks.create(Truck(plateNumber = "ABC-124", type = Truck.Type.SEMI_TRUCK, vin = "0001"))
-        val towable = it.user.towables.create(Towable(plateNumber = "ABC-123", type = Towable.Type.TRAILER, vin = "0000"))
-
-        it.user.telematics.receiveTelematicData(
-            vin = towable.vin!!,
-            telematicData = temelaticData
-        )
 
         it.user.telematics.receiveTelematicData(
             vin = truck.vin!!,
