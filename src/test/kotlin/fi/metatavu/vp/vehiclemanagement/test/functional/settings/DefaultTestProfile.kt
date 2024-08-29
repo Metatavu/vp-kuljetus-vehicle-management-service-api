74544140c20a0785d63bfef8bef53f22b04ea9df
Ville Juutila <ville.juutila@gmail.com>
2024-08-29 12:52:43 +0300

    Removed commented test configuration
    - Also moved rabbit mq connector configuration to application.properties

--- src/test/kotlin/fi/metatavu/vp/vehiclemanagement/test/functional/settings/DefaultTestProfile.kt
+++ src/test/kotlin/fi/metatavu/vp/vehiclemanagement/test/functional/settings/DefaultTestProfile.kt
@@ -13,13 +13,6 @@ class DefaultTestProfile: QuarkusTestProfile {
         val config: MutableMap<String, String> = HashMap()
         config["vp.vehiclemanagement.telematics.apiKey"] = VEHICLE_MANAGEMENT_TELEMATICS_API_KEY
 
-        config["mp.messaging.incoming.vp-in.connector"] = "smallrye-rabbitmq"
-//        config["mp.messaging.incoming.vp-in.queue.name"] = "incoming_queue"
-//        config["mp.messaging.incoming.vp-in.queue.x-queue-type"] = "quorum"
-//        config["mp.messaging.incoming.vp-in.exchange.name"] = EXCHANGE_NAME
-//        config["mp.messaging.incoming.vp-in.routing-keys"] = "DRIVER_WORKING_STATE_CHANGE"
-
-        config["mp.messaging.outgoing.vp-out.connector"] = "smallrye-rabbitmq"
         config["mp.messaging.outgoing.vp-out.exchange.name"] = EXCHANGE_NAME
 
         config["vp.env"] = "TEST"
